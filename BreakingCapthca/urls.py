1173e73cf79be2e7f3fc81e48f398a5192accf65
AAlkinani1986 <90129610+AAlkinani1986@users.noreply.github.com>
2023-04-26 10:41:41 +1000

    Upload image to the server

--- BreakingCapthca/urls.py
+++ BreakingCapthca/urls.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from objectDetection import views
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
@@ -11,4 +13,4 @@ urlpatterns = [
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls')),
     path('upload-image/', views.uploadFile, name='uploadFile'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- objectDetection/views.py
+++ objectDetection/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+import os
+import datetime
+import pytz
+from django.core.files.storage import FileSystemStorage
 
 
 def home(request):
@@ -26,6 +30,14 @@ def uploadFile(request):
         uploaded_file = request.FILES['file']
         print(uploaded_file.name)
         print(uploaded_file.size)
+        username = request.user.username
+        # replace with your timezone
+        local_tz = pytz.timezone('Australia/Sydney')
+        date = datetime.datetime.now(local_tz).strftime("%Y-%m-%d_%H-%M-%S")
+        filename, file_extension = os.path.splitext(uploaded_file.name)
+        new_filename = f"{username}_{date}{file_extension}"
+        print(new_filename)
         # Do something with the uploaded file here, e.g. save it to a folder or database
-
+        fs = FileSystemStorage()
+        fs.save(new_filename, uploaded_file)
         return HttpResponse(status=200)
